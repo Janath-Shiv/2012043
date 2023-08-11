87c5c7fd36fbb96efecc6eb528984c7f8a2c0de3
Janath-Shiv <129955205+Janath-Shiv@users.noreply.github.com>
2023-08-11 10:57:45 +0530

    Delete routes directory

--- routes/route1.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const {register,authentication} = require('../controller/controller1');
-router.route("/register").post(register);
-router.route("/auth").post(authentication);
-
-
-
-module.exports = router
--- routes/route2.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const {getAll,train,getOne} = require('../controller/controller2');
-router.route("/trains").get(getAll);
-router.route("/trains").post(train);
-router.route("/:id").get(getOne);
-
-
-module.exports = router
